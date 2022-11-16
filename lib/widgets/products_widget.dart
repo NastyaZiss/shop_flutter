ffaba599232651feb9b5e54c93be0cb11a36305e
NastyaZiss <kurbatova.vladuslava050@gmail.com>
2022-11-16 10:37:03 +0300

    Start products_widget Text and IconButton favorite.

--- lib/widgets/products_widget.dart
+++ lib/widgets/products_widget.dart
@@ -13,6 +13,9 @@ class ProductsWidget extends StatefulWidget {
 }
 
 class _ProductsWidgetState extends State<ProductsWidget> {
+
+  bool click = true;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,44 +23,75 @@ class _ProductsWidgetState extends State<ProductsWidget> {
       child: Stack(
         children: [
           Container(
-            child:
-            Column(
-              children: [
-                Row(
-
-                ),
-              ],
-            )
-          ),
-          Container(
             width: 180,
             height: 200,
             decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
             ),
           ),
           Positioned(
             left: 0,
-            top: 70,
+            top: 50,
             //child: Transform.rotate(
-              //angle: pi/4,
-              child: Container(
-                width: 182,
-                height: 190,
-                decoration: BoxDecoration(
-                  color: AppColors.ProductColorSh1,
-                  borderRadius: BorderRadius.only(
-                    // bottomLeft: Radius.circular(50),
-                    topLeft: Radius.circular(70),
-                    topRight: Radius.circular(20),
-                    // bottomLeft: Radius.circular(40),
-                    // bottomRight: Radius.circular(40),
-                    // bottomRight: Radius.elliptical(50, 40),
-                  ),
+            //angle: pi/4,
+            child: Container(
+              width: 180,
+              height: 150,
+              decoration: BoxDecoration(
+                color: AppColors.ProductColorSh1,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(85),
+                  topRight: Radius.circular(15),
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                  // bottomRight: Radius.elliptical(50, 40),
                 ),
               ),
             ),
+          ),
+          Container(width: 180,height: 55,
+            // alignment: Alignment.bottomRight,
+            // decoration: BoxDecoration(color: Colors.red),
+              child:
+              Padding(
+                padding: const EdgeInsets.only(left: 15,bottom: 8,right: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("top on the month", style: AppColors.h_3,),
+                          Text("Air Max 270 G",style: AppColors.h_1,),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      // decoration: BoxDecoration(color: Colors.blueAccent),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 70),
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              click = !click;
+                            });
+                          },
+                          icon: Icon(
+                            (click == false)
+                                ? Icons.favorite
+                                : Icons.favorite_border_outlined,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          ),
           //),
 
         ],
